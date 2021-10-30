126d520b68de8e646e2423450b7d694b8f8a431d
busyfingers <n.malmqvist@gmail.com>
2021-10-30 19:50:19 +0200

    Fixed issue where beforeEach ran too many times

--- runner.js
+++ runner.js
@@ -8,18 +8,18 @@ const forbiddenDirs = ['node_modules'];
 class Runner {
   constructor() {
     this.testFiles = [];
-    const beforeEaches = [];
+    this.beforeEaches = [];
     this.tests = [];
 
     global.render = render;
 
     global.beforeEach = fn => {
-      beforeEaches.push(fn);
+      this.beforeEaches.push(fn);
     };
 
     global.it = (desc, fn) => {
       this.tests.push(async () => {
-        beforeEaches.forEach(func => func());
+        this.beforeEaches.forEach(func => func());
 
         try {
           await fn();
@@ -35,6 +35,7 @@ class Runner {
 
   async runTests() {
     for (let file of this.testFiles) {
+      this.beforeEaches = [];
       this.tests = [];
       console.log(chalk.gray(`---- ${file.shortName}`));
 
